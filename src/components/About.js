db793b4c284d495a89c4d48bc41d7efc769cb4bb
John Doe <johndoe@example.com>
2023-05-31 07:18:33 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react'
+import Contact from '../../lib/Contact'
+
+const About = () => {
+    console.log("newwwwwwwwwwwwwwwwwwwwww")
+  return (
+    <div>About
+        <Contact />
+    </div>
+  )
+}
+
+export default About
